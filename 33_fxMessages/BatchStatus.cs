65a53175377788a6269e3ea8992d31b502544a21
Tony <tonyfreemind@msn.com>
2021-04-24 15:19:48 +0800

    Moved all the files step by step to GitHub

---
+++ 33_fxMessages/BatchStatus.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+using Ecng.Common;
+using Ecng.Serialization;
+
+using StockSharp.Localization;
+
+namespace fx.Messages
+{	
+	/// <summary>
+	/// Candle states.
+	/// </summary>
+	[System.Runtime.Serialization.DataContract]
+	[Serializable]
+	public enum BatchStatus : byte
+	{
+		/// <summary>
+		/// Single Message
+		/// </summary>
+		[EnumMember]
+		FromStorage,
+
+		/// <summary>
+		/// Message Batching Begin
+		/// </summary>
+		[EnumMember]
+		BeginBatch,
+
+		/// <summary>
+		/// Still Batching
+		/// </summary>
+		[EnumMember]
+		Batching,
+
+		/// <summary>
+		/// End of current Message Batch
+		/// </summary>
+		[EnumMember]
+		EndBatch,
+
+		/// <summary>
+		/// End of current Message Batch
+		/// </summary>
+		[EnumMember]
+		Latest,
+	}
+}
---
+++ 33_fxMessages/fxMessageTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fx.Messages
+{
+    public enum fxMessageTypes
+    {
+        fxCandleTimeFrame = 99
+    }
+
+    public enum fxMarketDataType
+    {
+        fxCandleTimeFrame = 99
+    }
+}
